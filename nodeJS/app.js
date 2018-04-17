6e0961faff34036c814a0947940d3c55eafa6cb1
rayanBS <rayan.paris.9@gmail.com>
2018-04-17 20:55:16 +0200

    add files for test nodeJS

---
+++ nodeJS/app.js
@@ -0,0 +1,41 @@
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+
+let server = http.createServer();
+
+server.on("request", (request, response) => {
+
+	response.writeHead(200);
+
+	let query = url.parse(request.url, true).query;
+	if(query.name == undefined){
+		response.write("Bonjour Anonyme !");
+	}
+	else{
+		response.write("Bonjour " + query.name + " !");
+	}
+	response.end();
+
+	/*
+	fs.readFile("index.html", (err, data) => {
+		if(err){
+			response.writeHead(404);
+			response.end("Ce fichier n'existe pas !");
+		}
+		else{
+			response.writeHead(200, {
+				"Content-type": "text/html; charset=utf-8"
+			});
+			response.end(data);
+		}
+	});
+	*/
+
+
+	
+	
+});
+
+
+server.listen(8080);
